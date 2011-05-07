805e6938819a7080f9abc167d1c37d4602e258c9
Bobby Impollonia <bobby@affinesystems.com>
2011-05-07 13:17:14 -0700

    Fully working SSE version with timings

--- sse.cpp
+++ sse.cpp
@@ -1,73 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <time.h>
 
+long count_bits_fast(unsigned char *buffer, size_t bufsize);
+long count_bits_naive(unsigned char *buffer, size_t bufsize);
+// helper for count_bits_fast
+inline long count_bits_sse(unsigned char *buffer, size_t bufsize);
 
-
-
-long count_bits_sse(void *buffer, size_t bufsize);
-long count_bits_naive(void *buffer, size_t bufsize);
-
-// helpers for count_bits_sse
-inline long count_bits_aligned(void *buffer, size_t bufsize);
-inline int count_bits_int_sse(unsigned int the_int);
-
-inline long count_bits_aligned(void *buffer, size_t bufsize)
+inline long count_bits_sse(unsigned char *buffer, size_t bufsize)
 {
-    const int iterations = bufsize / sizeof(unsigned);
+    const int iterations = bufsize / sizeof(unsigned long);
     if (!iterations)
         return 0;
-    int bitcount = 0;
-    int total;
+    long bitcount = 0;
+    long total;
     int ecx;
     __asm__ (
         "1:"
         "popcnt (%1), %0;"
-        "add %0, %7;"
+        "add %0, %3;"
         "add %5, %1;"
         "loop 1b;"
         : "=&r" (bitcount), "=&r" (buffer), "=&c" (ecx), "=&r" (total)
-        : "1" (buffer), "i" (sizeof(unsigned)), "2" (iterations), "3" (total)
+        : "1" (buffer), "i" (sizeof(unsigned long)), "2" (iterations), "3" (total)
         : "cc"
     );
     return total;
 }
 
-long count_bits_sse(void *buffer, size_t bufsize)
+long count_bits_fast(unsigned char *buffer, size_t bufsize)
 {
-    long bitcount = count_bits_aligned(buffer, bufsize);
-    const size_t num_ints = bufsize / sizeof(int);
-    const size_t num_chars= bufsize % sizeof(int);
-    unsigned char *the_char = static_cast<unsigned char *>(buffer) + num_ints * sizeof(int);
-    for (size_t which_char = 0; which_char < num_chars; which_char++)
-    {
-        bitcount += count_bits_int_sse(*the_char++);
-    }
+    const size_t aligned_size = (bufsize / sizeof(unsigned long)) * sizeof(unsigned long);
+    long bitcount = count_bits_sse(buffer, bufsize);
+    bitcount += count_bits_naive(buffer + aligned_size, bufsize - aligned_size);
     return bitcount;
 }
 
-int count_bits_int_sse(unsigned int the_int)
-{
-    int bitcount = 0;
-    __asm__ ( 
-        "popcnt %1, %0;"
-
-        : "=r" (bitcount)
-        :  "r" (the_int)
-        : "cc"
-    );
-    return bitcount;
-}
-
-long count_bits_naive(void *buffer, size_t bufsize)
+long count_bits_naive(unsigned char *buffer, size_t bufsize)
 {
     long bitcount = 0;
-    unsigned char *bytes = static_cast<unsigned char*>(buffer);
     for(size_t byte = 0; byte < bufsize; byte++)
         for(int bit = 0; bit < 8; bit++)
-            if (bytes[byte] & (1 << bit))
+            if (buffer[byte] & (1 << bit))
                 bitcount++;
     return bitcount;
 }
@@ -75,7 +50,7 @@ long count_bits_naive(void *buffer, size_t bufsize)
 int main(int argc, char **argv)
 {
     const char *filename = "/dev/urandom";
-    const size_t bufsize = 100000000; 
+    size_t bufsize = 2000000000; 
     unsigned char *buffer = new unsigned char[bufsize];
 
     printf("Reading input...\n");
@@ -84,11 +59,17 @@ int main(int argc, char **argv)
     fclose(infile);
     printf("done reading input\n");
 
-    int iters = 10;
+    // Let's make the data unaligned so it's even harder for SSE
+    // who sometimes cares about such things
+    buffer += 1;
+    bufsize -= 1;
+
+    int iters;
     time_t start, duration;
 
     printf("Timing naive implementation\n");
     start = time(NULL);
+    iters = 3;
     for (int i =0; i < iters; i++)
     {
         long num_bits = count_bits_naive(buffer, bufsize);
@@ -104,13 +85,13 @@ int main(int argc, char **argv)
 
     printf("Timing badass implementation\n");
     start = time(NULL);
-    iters = 100;
+    iters = 10000;
     for (int i =0; i < iters; i++)
     {
-        long num_bits = count_bits_sse(buffer, bufsize);
+        long num_bits = count_bits_fast(buffer, bufsize);
         if (i == 0)
             printf("%ld bits are set", num_bits);
-        else
+        else if (! (i % 100))
             printf(".");
         fflush(stdout);
     }
